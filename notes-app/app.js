c53c160273aafecb1016aa865d8ebaf377e14f46
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-01-30 15:23:30 -0500

    12 printing in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const msg = getNotes();
 console.log(msg);
 
-console.log(validator.isURL('http/mead.io'));
+console.log(chalk.bgGreen.bold('Success!'));
