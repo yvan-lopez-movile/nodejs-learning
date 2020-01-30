1462cf72d47783cc566e717f409856d47981ed50
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-01-30 14:19:04 -0500

    10 Importing own files

--- app.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-// fs.writeFileSync('notes.txt', 'My name is Yvan');
-
-fs.appendFileSync('notes.txt', ' and I\'m a senior developer.' );
---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const add = require('./utils');
+
+const sum = add(4, -2);
+
+console.log(sum);
