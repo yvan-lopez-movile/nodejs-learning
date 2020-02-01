3fc53e0f347456837efff2e5087b6abf2c1fb6bf
Yvan Lopez <yvan.lopez@wavy.global>
2020-02-01 00:57:10 -0500

    39

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,35 @@
+// Object property shorthand
+
+const name = 'Yvan';
+const userAge = 37;
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Chimbote'
+}
+
+console.log(user);
+
+// Object destructering
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salesPrice: undefined,
+    rating: 4.2
+}
+
+// const label = product.label;
+// const stock = product.stock;
+
+// const {label: productLabel, stock, rating = 5} = product;
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product);
