724063401e271bf376e1412838358ed466499e9d
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-01 11:57:00 -0500

    44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,20 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
-})
+    res.send([
+        {
+            name: 'Yvan',
+            age: 37
+        },
+        {
+            name: 'Benji',
+            age: 7
+        }])
+});
 
 app.get('/about', (req, res) => {
-    res.send('About page')
-})
+    res.send('<h1>About page</h1>')
+});
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
-})
+    res.send({
+        forecast: 'Todo el dia soleado',
+        location: 'Chimbote'
+    })
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
