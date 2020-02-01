edeea451a300af3698c1e6f5dee2f27bfbe146c1
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-01 12:55:37 -0500

    47

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,30 @@ const express = require('express');
 
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Yvan Lopez'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Acerca de mi',
+        name: 'Yvan Lopez'
+    })
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'Estamos para ayudarte'
+    })
+});
 
 app.get('/weather', (req, res) => {
     res.send({
