5f1657b47b3c022c4475ccceb9052793a4cce949
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-01 12:33:22 -0500

    46

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,9 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
-
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-});
-
-app.get('/help', (req, res) => {
-    res.send([
-        {
-            name: 'Yvan',
-            age: 37
-        },
-        {
-            name: 'Benji',
-            age: 7
-        }])
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About page</h1>')
-});
+const publicDirPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirPath));
 
 app.get('/weather', (req, res) => {
     res.send({
