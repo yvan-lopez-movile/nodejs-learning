08af234660d44201d486e0b0afabca555124ae5f
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-01 13:11:03 -0500

    48

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,16 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirPath = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// setup static directory to serve
 app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
