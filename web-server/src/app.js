6bdcfe606c3aa0a0982c7bf2bc7ee21b59e50322
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-01 14:44:30 -0500

    50

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.use(express.static(publicDirPath));
@@ -31,7 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        msg: 'Estamos para ayudarte'
+        msg: 'Estamos para ayudarte',
+        name: 'Yvan Lopez'
     })
 });
 
