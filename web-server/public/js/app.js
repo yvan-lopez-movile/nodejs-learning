cecb2d7761800f8035f4d499867ff8348bc501a0
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-03 13:08:54 -0500

    setup for heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     msgTwo.textContent = ''
     const location = search.value;
 
-    fetch('http://localhost:3000/weather/?address=' + location).then((response) => {
+    fetch('/weather/?address=' + location).then((response) => {
         response.json().then((data) => {
             msgOne.textContent = ''
             if (data.error) {
