f86257934e1d434cd9c29d63d796d3a18381504e
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-04 13:08:45 -0500

    77

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,14 @@
 // CRUD: create, read, update, delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString())
+// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true},
     (error, client) => {
@@ -14,9 +18,54 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 
         const db = client.db(databaseName)
 
-        db.collection('users').insertOne({
-            name: 'Yvan',
-            age: 27
-        })
+        // db.collection('users').insertOne({
+        //     _id: id,
+        //     name: 'Anthony',
+        //     age: 36
+        // }, (error, result) => {
+        //     if (error) {
+        //         return console.log('Unable to insert user!')
+        //     }
+        //
+        //     console.log(result.ops)
+        // })
+
+        // db.collection('users').insertMany([
+        //     {
+        //         name: 'Ben',
+        //         age: 7
+        //     },
+        //     {
+        //         name: 'Fausto',
+        //         age: 1
+        //     }
+        // ], (error, result) => {
+        //     if (error) {
+        //         return console.log('Unable to insert documents!')
+        //     }
+        //
+        //     console.log(result.ops)
+        // })
+
+        // db.collection('tasks').insertMany([
+        //     {
+        //         description: 'tarea 1',
+        //         completed: true
+        //     },
+        //     {
+        //         description: 'tarea 2',
+        //         completed: true
+        //     },
+        //     {
+        //         description: 'tarea 3',
+        //         completed: false
+        //     }
+        // ], (error, result) => {
+        //     if (error) {
+        //         return console.log('Unable to insert tasks!')
+        //     }
+        //
+        //     console.log(result.ops)
+        // })
 
     })
