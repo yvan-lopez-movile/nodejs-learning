5efb1520db4a0528afb0a837f3c86192510e05f7
yvan-lopez-movile <yvan.lopez@wavy.global>
2020-02-04 17:12:12 -0500

    79 promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,33 +1,15 @@
-// setTimeout(() => {
-//     console.log("Two seconds are up");
-// }, 2000);
-//
-// const names = ['Yvan', 'Antony', 'Benjamin'];
-//
-// const shortNames = names.filter((name) => name.length <= 4);
-//
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-//         callback(data);
-//
-//     }, 2000);
-// };
-//
-// geocode('Chimbote', (data) => {
-//     console.log(data);
-// });
-
-const add = (x, y, callback) => {
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        const sum = x + y;
-        callback(sum);
+        // callback('This is my error', undefined)
+        callback(undefined, [1, 4, 7])
     }, 2000)
+
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
-});
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
+})
