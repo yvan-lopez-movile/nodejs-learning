4c98252c2966643581b1d1f2316c3d87c5c0f190
Yvan Lopez <yvan.lopez@wavy.global>
2020-02-05 00:05:04 -0500

    86 challenge - my solution

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -6,25 +7,59 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-//
-// const me = new User({
-//     name: 'Yvan',
-//     age: 37 //'any_string'
-// })
-//
-// me.save().then((me)=>{
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log('Error!', error)
-// })
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (value.length < 7) {
+                throw new Error('Password must be greater than 6 characters')
+            }
+
+            if (value.includes('password')) {
+                throw new Error('The password shouldn\'t contain the "password" word')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: '   Benji    ',
+    email: 'BenjI@MAIL.COM   ',
+    password: 'uno'//'unpassword'
+})
+
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
 
 const Task = mongoose.model('Task', {
     description: {
@@ -34,14 +69,14 @@ const Task = mongoose.model('Task', {
         type: Boolean
     }
 })
-
-const task = new Task({
-    description: 'Clean the house',
-    completed: true
-})
-
-task.save().then((taskResult) => {
-    console.log(taskResult)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+//
+// const task = new Task({
+//     description: 'Clean the house',
+//     completed: true
+// })
+//
+// task.save().then((taskResult) => {
+//     console.log(taskResult)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
