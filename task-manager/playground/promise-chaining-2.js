c887ede2219ab8af0102b470e3a5fd164189e8ff
Yvan Lopez <yvan.lopez@wavy.global>
2020-02-19 10:19:03 -0300

    96

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,10 +1,22 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('abc123').then((task) => {
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('abc123').then((task) => {
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id)
+    return await Task.documents({completed: false})
+}
+
+deleteTaskAndCount('123abc').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
+
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,22 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('abc123', {age: 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({age: 1})
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('abc123', {age: 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({age: 1})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    return await User.documents({age})
+}
+
+updateAgeAndCount('abc123', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
