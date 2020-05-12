e190bd8464bedb37fe836930120a5e8dcbec7746
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-12 00:34:35 -0500

    110

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -14,13 +15,8 @@ router.post('/users', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.post('/users/login', async (req, res) => {
