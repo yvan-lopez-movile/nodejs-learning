ba3fbd6a1430cb9e35bce43ef38ba7ade0c44a8a
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-14 22:15:24 -0500

    114

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,3 +25,20 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('5ebe01cb8a21241d5e0f0208')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5ebe001c9efcef1b341c2f77')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+
+    
+}
+
+main()
