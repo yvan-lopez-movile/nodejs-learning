4cf7f56dd55f547ea499a688e219edba08455046
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-16 15:48:10 -0500

    120

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,7 +18,8 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-// GET /tasks?completed=true 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
 
     const match = {}
@@ -30,7 +31,11 @@ router.get('/tasks', auth, async (req, res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
