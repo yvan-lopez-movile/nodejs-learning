1383609d67a6e5336d6134c93ba64f6e94ec4541
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-16 16:06:08 -0500

    121

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,12 +20,19 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=20
+// GET /task?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
 
     const match = {}
+    const sort = {}
 
-    if(req.query.completed){
+    if (req.query.completed){
         match.completed = req.query.completed === 'true'
+    }
+
+    if ( req.query.sort ) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -34,7 +41,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
