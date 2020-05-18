28c7f8f59e08f1fb3511d56dda5db968310364c9
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-18 11:26:06 -0500

    141

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -2,20 +2,20 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const sendWelcomeEmail = ( email, name ) => {
+const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'yvan.lopez.it@gmail.com',
+        from: 'andrew@mead.io',
         subject: 'Thanks for joining in!',
-        text: `Welcome to the app, ${name}. Let me know when you get along with the app.`
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
 }
 
-const sendCancelationEmail = ( email, name ) => {
+const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'yvan.lopez.it@gmail.com',
-        subject: 'Sorry to see you go',
+        from: 'andrew@mead.io',
+        subject: 'Sorry to see you go!',
         text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
 }
