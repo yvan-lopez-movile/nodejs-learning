a6192d4b9a3f50f6f8048e7358ad9113e9bf0113
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-19 20:35:29 -0500

    162

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,34 +2,41 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
-    console.log('New websocket connection')
+    console.log('New WebSocket connection')
 
-    const msg = 'Welcome!'
-    socket.emit('welcomeMsg', msg)
+    socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (sendMessage) => {
-        io.emit('sendMessage', sendMessage)
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('A user has left!')
+        io.emit('message', 'A user has left!')
     })
 })
 
