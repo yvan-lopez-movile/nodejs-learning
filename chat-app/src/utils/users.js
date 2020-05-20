9a7999ffda360e17c1827b23cceefc1c9d3c991e
Yvan Lopez <yvan.lopez@wavy.global>
2020-05-19 22:40:36 -0500

    168

---
+++ chat-app/src/utils/users.js
@@ -0,0 +1,46 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username: username.trim().toLowerCase()
+    room: room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user)=> {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if(existingUser){
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store User
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+module.exports = {
+    addUser,
+    removeUser
+}
